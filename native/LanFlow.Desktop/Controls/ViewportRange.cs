a0aa989986129938403f168f8b7f627ed0baec9a
Zhang San <24036856@qq.com>
2026-07-30 18:35:54 +0800

    feat: add virtual wrap layout geometry

---
+++ native/LanFlow.Desktop/Controls/ViewportRange.cs
@@ -0,0 +1,20 @@
+﻿namespace LanFlow.Desktop.Controls;
+
+public readonly record struct ViewportRange(
+    int FirstIndex,
+    int LastIndex,
+    int Columns)
+{
+    public static ViewportRange Empty => new(-1, -1, 1);
+
+    public bool Contains(int index) =>
+        FirstIndex >= 0 && index >= FirstIndex && index <= LastIndex;
+}
+
+public enum NavigationDirection
+{
+    Left,
+    Right,
+    Up,
+    Down
+}
